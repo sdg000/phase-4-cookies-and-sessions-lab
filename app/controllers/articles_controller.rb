82ea41b44d715ae41706139123580b86fa609560
Seth David Gyimah <esec26@gmail.com>
2022-10-18 16:58:39 +0000

    completed all lab requirements

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,32 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    #step 1: when get show/:id request hits #articles_controller
+        # find the the instance matching the [:id]
     article = Article.find(params[:id])
-    render json: article
+
+    #if the instance is found, 
+    if article
+
+      # start keeping track of page_views using session_method
+      session[:page_views] ||= 0
+
+      #increment page_view tracker by 1, when user "gets" another show/:id
+      session[:page_views] += 1
+
+      #whiles page_views is less than 3, render article
+      if session[:page_views] <= 3
+        render json: article
+      else
+        #when page_view tracker is 3 or more:
+        render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+      end
+
+
+    end
+  
+
+
   end
 
   private
